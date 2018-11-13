1600001ee43d7514d723e293e28bdf7665c806cf
95clemente <44986718+95clemente@users.noreply.github.com>
2018-11-12 22:35:07 -0500

    Add files via upload

---
+++ Contractor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project331;
+
+
+public class Contractor extends Customer 
+{
+    public String contractorName; 
+    public long contractorNumber; 
+    public String contractorAddress; 
+    
+public Contractor()
+{
+   super();
+   this.contractorName="";
+   this.contractorAddress="";
+   this.contractorNumber = 0; 
+   
+}
+    public Contractor (String fName, String lName)
+    {
+        super (fName); 
+    }
+}
