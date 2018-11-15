9129f866152a8ce593b4da2fcc18c3150e7d64b4
95clemente <44986718+95clemente@users.noreply.github.com>
2018-11-14 20:19:07 -0500

    Update Contractor.java

--- Contractor.java
+++ Contractor.java
@@ -11,30 +11,37 @@ public class Contractor extends Customer
     public String contractorName; 
     public long contractorNumber; 
     public String contractorAddress; 
-    public int contractorID = 2000;
+    Customer firstName; 
     
-
-    public Contractor (String firstName, String lastName, String city, String state, int zip, String contractorName, long contractorNumber, String contractorAddress)
+public Contractor (String firstName, String lastName, String city, String street, 
+        String state, int zip, String contractorName, long contractorNumber, 
+        String contractorAddress)
     {
-        super (firstName);
-        super (lastName);
-        super (city);
-        super (state);
-        super(zip);
+        super(firstName, lastName, city, state, street, zip, contractorNumber);
         this.contractorName = contractorName;
         this.contractorNumber = contractorNumber;
         this.contractorAddress = contractorAddress; 
-        this.contractorID++;
     }
     
-    public void setContractorName (String contractorName) //setting the contractor company name
+    //setting the contractor company name
+    public void setContractorName (String contractorName) 
     {
-    this.contractorName = contractorName; 
+       this.contractorName = contractorName; 
+    }
+    public String setContractorAddress( String contractingAddress)
+    {
+        contractingAddress ="";
+        contractingAddress += this.street + ", "; 
+        contractingAddress += this.city + ", ";
+        contractingAddress += this.state + ", ";
+        contractingAddress += this.zip;
+        return contractingAddress; 
     }
     
-    public void setContractorNumber (long contractorNumber) //setting contractor company number
+    //setting contractor company number
+    public void setContractorNumber (long contractorNumber) 
     {
-    this.contractorNumber = contractorNumber; 
+        this.contractorNumber = contractorNumber; 
     }
     
     
