d93f4c3f103dbbb31a097fe3eeafb7687e43e93d
95clemente <44986718+95clemente@users.noreply.github.com>
2018-11-14 20:31:43 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ CIS 331 Section 2 
+ Group Project Part 1.
+ Authors: Zach Beatty, Eric Carter, Mercy Clemente, & Troy God
  */
 
 package project331;
