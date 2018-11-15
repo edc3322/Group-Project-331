3c1c5aabba6b0a40ab49044d35fc8528bf0c06f0
95clemente <44986718+95clemente@users.noreply.github.com>
2018-11-15 18:58:47 -0500

    Update Sale.java

--- Sale.java
+++ Sale.java
@@ -7,19 +7,20 @@
 package Project331;
 
 public class Sale {
-    private int saleID = 3000;
-    Item newItem;
-    public double sellingPrice;
-    public int quantity;
-    public String customer;
-    public String date;
+      private  int saleID = 3000;
+   Item newItem;
+   public double sellingPrice;
+   public int quantity;
+   public String customer;
+   public String date;
     
     public Sale(Item newItem)
     {
         this.newItem = newItem;
     }
     
-    public Sale(Item newItem, double sellingPrice, int quantity, String customer, String date)
+    public Sale(Item newItem, double sellingPrice, int quantity, String customer, 
+            String date)
     {
         this.newItem = newItem;
         this.sellingPrice = sellingPrice;
@@ -28,16 +29,18 @@ public class Sale {
         this.date = date;
         this.saleID++;
     }
- public int getId()
- {
-  return this.saleID++; 
- }
+    
+    public int getSaleID()
+    {
+        return this.saleID; 
+    }
     
     public void printReceipt()
     {
-        String saleReceipt = "Item Sold: " + newItem.getItemID() + ", " + 
-         Selling Price: $" + String.valueOf(this.sellingPrice) + ", Quantity: " 
-         + String.valueOf(quantity) + ", Sale Total: $" + String.valueOf(quantity * this.sellingPrice);
+        String saleReceipt = "Item Sold: " + newItem.getItemID() + ", Selling "
+        + "Price: $" + String.valueOf(this.sellingPrice) + ", Quantity: " + 
+        String.valueOf(quantity) + ", Sale Total: $" + String.valueOf(quantity 
+         * this.sellingPrice);
         System.out.println(saleReceipt);
     }
 
