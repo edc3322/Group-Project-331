4d3f5332948dcd31358ddef33bedaf2bb5804c9d
95clemente <44986718+95clemente@users.noreply.github.com>
2018-11-18 14:54:03 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -7,29 +7,56 @@ package Project331;
 
 public class Item 
 {
-private int itemID = 4000;
+private int itemID;
 public String itemName;
 public double weight;
 public String description;
 private double purchasePrice;
-public double sellingPrice; 
+public double sellingPrice;
 
-public Item(String itemName, double weight, String description, double 
-    purchasePrice, sellingPrice)
+public Item(int itemID, String itemName, double weight, String description, 
+        double purchasePrice, double sellingPrice)
 {
 	this.itemName = itemName;
 	this.weight = weight;
-	this. description = description;
+	this.description = description;
 	this.purchasePrice = purchasePrice;
-	this.sellingPrice = sellingPrice;
-	this.itemID++;
+        this.sellingPrice = sellingPrice; 
+	this.itemID=itemID;
 }
-	
+//setter for item name
+public void setName (String itemName)
+{
+    this.itemName=itemName;
+}
+
+public Item getItemName()
+{
+    return this.getItemName();
+}
+public void setDescription (String description)
+{
+    this.description=description;
+}
+
+public String getDescription()
+{
+    return this.description;
+}
 public int getItemID()
 {
     return this.itemID;
 }
-	
+
+public void setSellingPrice (double sellingPrice)
+{
+    this.sellingPrice=sellingPrice;
+}
+
+public double getSellingPrice()
+{
+    return this.sellingPrice;
+}
 public boolean setpurchasePrice(double purchasePrice)
 {
     if (purchasePrice > 0)
@@ -45,14 +72,22 @@ public double getpurchasePrice()
     return purchasePrice;
 }
 
+public void setWeight(double weight)
+{
+    this.weight=weight;
+}
 
+public double getWeight()
+{
+    return this.weight;
+}
 	@Override
 public String toString()
 {
-    String returnString = String.format("%-12d" + " %-16s" + " %-16f" + " %-20s" + " %-12f" + " %-12f",
-    this.itemID, this.itemName, this.weight, this.description, this.purchasePrice, this.sellingPrice);
+    String returnString = String.format("%-12d" + " %-16s" + " %-16f" + " %-20s" 
+    + " %-12f" + " %-12f", this.itemID, this.itemName, this.weight, 
+    this.description, this.purchasePrice, this.sellingPrice);
     return returnString;
 }
 	
 }
-
