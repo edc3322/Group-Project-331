4b52ba5e77ca313a3a9f81b0f8c067fc0b9e70f7
95clemente <44986718+95clemente@users.noreply.github.com>
2018-12-04 14:00:26 -0500

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -33,6 +33,43 @@ public class MainApp extends Application {
     TextField txtSellingPrice = new TextField();
     TextField txtQuantity = new TextField();
     TextField txtCustomerName = new TextField();
+    //Setting dimensions for textfields
+    txtFName.setMaxWidth(200);
+    txtLName.setMaxWidth(200);
+    txtStreet.setMaxWidth(200);
+    txtCity.setMaxWidth(200);
+    txtZip.setMaxWidth(200);
+    txtPhone.setMaxWidth(200);
+    txtItem.setMaxWidth(200);
+    txtWeight.setMaxWidth(200);
+    txtDescription.setMaxWidth(200);
+    txtPurchasePrice.setMaxWidth(200);
+    txtSellingPrice.setMaxWidth(200);
+    txtQuantity.setMaxWidth(100);
+    txtCustomerName.setMaxWidth(200);
+    
+   TextArea taReceipt = new TextArea(); 
+   TextArea taInventory = new TextArea();
+   TextArea taCustPurchHistory = new TextArea();
+   TextArea taNotes = new TextArea();
+   TextArea taItemHistory = new TextArea();
+   ScrollPane itemHistoryScrollPane = new ScrollPane(taItemHistory);
+   ScrollPane notesScrollPane = new ScrollPane(taNotes);
+   ScrollPane receiptScrollPane = new ScrollPane (taReceipt);
+   ScrollPane inventoryScrollPane = new ScrollPane(taInventory);
+   ScrollPane custPurchHistoryPane = new ScrollPane(taCustPurchHistory);
+   notesScrollPane.setPrefSize(350,150);
+   custPurchHistoryPane.setPrefSize(400,250);
+   inventoryScrollPane.setPrefSize(400,250);
+   receiptScrollPane.setPrefSize(400,250);
+   itemHistoryScrollPane.setPrefSize(400,250);
+   
+   taReceipt.setPrefSize(400,250);
+   taInventory.setPrefSize(400,250);
+   taCustPurchHistory.setPrefSize(400,250);
+   taNotes.setPrefSize(350,150);
+   taItemHistory.setPrefSize(400,250);
+   
     Label lblItem = new Label("Item Name: ");
     Label lblWeight = new Label("Item Weight: ");
     Label lblDescription = new Label ("Item Description: ");
@@ -48,18 +85,68 @@ public class MainApp extends Application {
     Label lblNotes = new Label("Enter Any Necessary Notes:");
     Label lblQuantity = new Label("Quantity:");
     Label lblCustomerName = new Label("Customer Name:");
-    TextArea taNotes = new TextArea();
-    ScrollPane notesScrollPane = new ScrollPane(taNotes);
+    Label lblItemHistory = new Label("Viewing Item Purchase History");
+
     ObservableList olState = FXCollections.observableArrayList();
-    
-    Button btnCustPurchase = new Button("Go to Customer Purchase History"); 
-    Button btnItemPurch = new Button("Go to Item Purchase History");
-    Button btnCurrentInv = new Button ("Go to Current Inventory Levels");
+  
+//Declaring all buttons 
+    Button btnCustPurchase = new Button("View Customer Purchase History"); 
+    Button btnItemPurch = new Button("View Item Purchase History");
+    Button btnCurrentInv = new Button ("View Current Inventory Levels");
     Button btnCreateMenu = new Button ("Go to the 'Create' Menu");
     Button btnEdit = new Button ("Go to  the 'Edit' Menu"); 
     Button btnPrint = new Button("Print Receipt");
     Button btnExit = new Button ("Exit"); 
-    ComboBox cmboStates = new ComboBox(olState);    
+    Button btnCustPurchExit = new Button("Exit");
+    Button btnPrintExit = new Button ("Exit");
+    Button btnEditExit = new Button("Exit");
+    Button btnEditCustomer = new Button("Edit Customer");
+    Button btnEditItem = new Button ("Edit Item");
+    Button btnEditSale = new Button ("Edit Sale");
+    Button btnEditVendor = new Button ("Edit Vendor"); 
+    Button btnExitCust = new Button("Exit");
+    Button btnSaveCustInfo = new Button("Save");
+    Button btnItemExit = new Button ("Exit"); 
+    Button btnSaleExit = new Button ("Exit");
+    Button btnVendorExit = new Button("Exit");
+    Button btnExitInventory = new Button("Exit");
+    Button btnCreateCust = new Button ("Create New Customer");
+    Button btnCreateItem = new Button ("Create New Item");
+    Button btnVendor = new Button ("Create New Vendor");
+    Button btnCreateSale = new Button ("Create New Sale");
+    Button btnExitCreate = new Button ("Exit");
+    Button btnItemHistoryExit = new Button("Exit");
+    
+    //Setting button dimensions 
+    btnCustPurchase.setPrefSize(250, 10);
+    btnItemHistoryExit.setPrefSize(100,10);
+    btnItemPurch.setPrefSize(250,10);
+    btnCurrentInv.setPrefSize(250,10);
+    btnCreateMenu.setPrefSize(250,10);
+    btnEdit.setPrefSize(250,10);
+    btnPrint.setPrefSize(250,10);
+    btnExit.setPrefSize(250,10);
+    btnCustPurchExit.setPrefSize(150,10);
+    btnPrintExit.setPrefSize(150,10);
+    btnEditExit.setPrefSize(250,10);
+    btnEditCustomer.setPrefSize(250,10);
+    btnEditItem.setPrefSize(250,10);
+    btnEditSale.setPrefSize(250,10);
+    btnEditVendor.setPrefSize(250,10);
+    btnExitCust.setPrefSize(250,10);
+    btnSaveCustInfo.setPrefSize(250,10);
+    btnItemExit.setPrefSize(200,10);
+    btnSaleExit.setPrefSize(200,10);
+    btnVendorExit.setPrefSize(250,10);
+    btnExitInventory.setPrefSize(250,10);
+    btnCreateCust.setPrefSize(250,10);
+    btnCreateItem.setPrefSize(250,10);
+    btnVendor.setPrefSize(250,10);
+    btnCreateSale.setPrefSize(250,10);
+    btnExitCreate.setPrefSize(250,10);
+    
+    ComboBox cmboStates = new ComboBox(olState); 
+    cmboStates.setPrefSize(200,10);
     taNotes.setWrapText(true);    
 //Filling observable list 
     olState.add("AL - Alabama");
@@ -117,29 +204,49 @@ public class MainApp extends Application {
         GridPane custPurchPane = new GridPane();
         custPurchPane.setAlignment(Pos.CENTER);
         Stage custPurchStage = new Stage();
-        Scene custPurchScene = new Scene(custPurchPane, 400,400);
+        Scene custPurchScene = new Scene(custPurchPane, 600,400);
         custPurchStage.setTitle("Customer Purchase History");
+        Label lblCustT = new Label("Viewing Customer Purchase History");
         custPurchStage.setScene(custPurchScene);
+        custPurchPane.add(lblCustT,0,0);
+        custPurchPane.add(custPurchHistoryPane,0,1);
+        custPurchPane.add(btnCustPurchExit,0,2);
+        btnCustPurchase.setOnAction(e ->{
+            custPurchStage.show(); 
+        });
+        btnCustPurchExit.setOnAction(e -> {
+            custPurchStage.close();
+        }); 
+        custPurchPane.setVgap(20);
+        custPurchPane.setHgap(20);
         
   //Creating the pane, stage, and scene for item purchase history
         GridPane itemPurchPane = new GridPane();
         itemPurchPane.setAlignment(Pos.CENTER);
         Stage itemPurchStage = new Stage();
-        Scene itemPurchScene = new Scene(itemPurchPane, 400,400);
+        Scene itemPurchScene = new Scene(itemPurchPane, 500,400);
         itemPurchStage.setTitle("Item Purchase History");
         itemPurchStage.setScene(itemPurchScene); 
+        itemPurchPane.add(itemHistoryScrollPane,0,1);
+        itemPurchPane.add(btnItemHistoryExit,0,2);
+        itemPurchPane.add(lblItemHistory,0,0);
+        btnItemPurch.setOnAction(e -> {
+            itemPurchStage.show();
+        });
+        btnItemHistoryExit.setOnAction(e -> {
+            itemPurchStage.close();
+        });
+        itemPurchPane.setVgap(20);
+        itemPurchPane.setHgap(20);
         
   //Creating the current inventory 
         GridPane currentInvPane = new GridPane();
         currentInvPane.setAlignment(Pos.CENTER);
         Stage currentInvStage = new Stage();
-        Scene currentInvScene = new Scene(currentInvPane, 400,400);
+        Scene currentInvScene = new Scene(currentInvPane, 500,400);
         currentInvStage.setTitle("Current Inventory Levels");
         currentInvStage.setScene(currentInvScene); 
-        TextArea taInventory = new TextArea();
-         Button btnExitInventory = new Button("Exit");
         Label lblInventory = new Label("Current Inventory Levels:");
-        ScrollPane inventoryScrollPane = new ScrollPane(taInventory);
         currentInvPane.add(lblInventory,0,0);
         currentInvPane.add(inventoryScrollPane,0,1);
         currentInvPane.add(btnExitInventory,0,2);
@@ -151,18 +258,14 @@ public class MainApp extends Application {
         });
         currentInvPane.setVgap(20);
         currentInvPane.setHgap(20);
-  //Create menu 
+  
+//Create menu 
         GridPane createPane = new GridPane();
         createPane.setAlignment(Pos.CENTER);
         Stage createStage = new Stage();
-        Scene createScene = new Scene(createPane, 400,400);
+        Scene createScene = new Scene(createPane, 500,400);
         createStage.setTitle("Create Menu Options");
         createStage.setScene(createScene); 
-        Button btnCreateCust = new Button ("Create New Customer");
-        Button btnCreateItem = new Button ("Create New Item");
-        Button btnVendor = new Button ("Create New Vendor");
-        Button btnCreateSale = new Button ("Create New Sale");
-        Button btnExitCreate = new Button ("Exit");
         createPane.add(btnCreateCust,1,0);
         createPane.add(btnCreateItem,1,1);
         createPane.add(btnVendor,1,2);
@@ -181,7 +284,6 @@ public class MainApp extends Application {
         Scene vendorScene = new Scene(vendorPane, 800,500);
         vendorStage.setTitle("Create Vendor");
         vendorStage.setScene(vendorScene);
-        Button btnVendorExit = new Button("Exit");
         vendorPane.add(btnVendorExit,1,7);
         Label lblNewT = new Label("Creating a New Vendor");
         vendorPane.add(lblNewT,0,7);
@@ -212,10 +314,9 @@ public class MainApp extends Application {
         GridPane salePane = new GridPane();
         salePane.setAlignment(Pos.CENTER);
         Stage saleStage = new Stage();
-        Scene saleScene = new Scene(salePane,400,400);
+        Scene saleScene = new Scene(salePane,500,400);
         saleStage.setTitle("Create New Sale");
         saleStage.setScene(saleScene);
-        Button btnSaleExit = new Button ("Exit");
         salePane.add(lblItem,0,0);
         salePane.add(lblSellingPrice,0,1);
         salePane.add(lblQuantity, 0,2); 
@@ -237,10 +338,9 @@ public class MainApp extends Application {
         GridPane createItemPane = new GridPane();
         createItemPane.setAlignment(Pos.CENTER);
         Stage createItemStage = new Stage();
-        Scene createItemScene = new Scene(createItemPane, 400,400);
+        Scene createItemScene = new Scene(createItemPane, 500,400);
         createItemStage.setTitle("Create New Item");
         createItemStage.setScene(createItemScene);
-        Button btnItemExit = new Button ("Exit"); 
         createItemPane.add(btnItemExit,1, 5);
         createItemPane.add(lblItem, 0,0);
         createItemPane.add(lblWeight,0,1);
@@ -266,12 +366,10 @@ public class MainApp extends Application {
         GridPane createCustPane = new GridPane();
         createCustPane.setAlignment(Pos.CENTER);
         Stage createCustStage = new Stage();
-        Scene createCustScene = new Scene(createCustPane, 800,650);
+        Scene createCustScene = new Scene(createCustPane, 660,600);
         createCustStage.setTitle("Create New Customer");
         createCustStage.setScene(createCustScene);
         Label lblNew = new Label ("CREATING NEW CUSTOMER");
-        Button btnExitCust = new Button("Exit");
-        Button btnSaveCustInfo = new Button("Save");
         createCustPane.add(lblNew,0,0);
         createCustPane.add(lblFName,0,1);
         createCustPane.add(lblLName,0,2);
@@ -308,11 +406,6 @@ public class MainApp extends Application {
         Scene editScene = new Scene(editPane, 400,400);
         editStage.setTitle("Edit Menu Options");
         editStage.setScene(editScene); 
-        Button btnEditExit = new Button("Exit");
-        Button btnEditCustomer = new Button("Edit Customer");
-        Button btnEditItem = new Button ("Edit Item");
-        Button btnEditSale = new Button ("Edit Sale");
-        Button btnEditVendor = new Button ("Edit Vendor"); 
         editPane.add(btnEditCustomer,0,0);
         editPane.add(btnEditItem, 0,1);
         editPane.add(btnEditSale,0,2);
@@ -334,11 +427,8 @@ public class MainApp extends Application {
         Scene printReceiptScene = new Scene(printReceiptPane, 550,400);
         printReceiptStage.setTitle("Print Receipt");
         printReceiptStage.setScene(printReceiptScene); 
-        TextArea taReceipt = new TextArea(); 
         taReceipt.setWrapText(true);
         Label lblTitle = new Label ("Printing Receipt");
-        Button btnPrintExit = new Button ("Exit");
-        ScrollPane receiptScrollPane = new ScrollPane (taReceipt);
         printReceiptPane.add(lblTitle,0,0);
         printReceiptPane.add(receiptScrollPane,0,1);
         printReceiptPane.add(btnPrintExit,0,2);
@@ -348,6 +438,8 @@ public class MainApp extends Application {
         btnPrintExit.setOnAction(e ->{
             printReceiptStage.close();
         });
+        printReceiptPane.setVgap(20);
+        printReceiptPane.setHgap(20);
 
   //Main Menu 
         Label lblMain = new Label (" Valley Depot's Main Menu"); 
