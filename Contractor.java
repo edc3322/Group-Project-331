9227dc57b66853d28e4d4c91dd245a125f0282bd
edc3322 <44984066+edc3322@users.noreply.github.com>
2018-12-06 22:19:43 -0500

    Update Contractor.java

--- Contractor.java
+++ Contractor.java
@@ -3,34 +3,44 @@ CIS 331 Section 2
 Group Project Part 1.
 Authors: Zach Beatty, Eric Carter, Mercy Clemente, Michael Corcoran & Troy Goddard
  */
-
-
 package GroupProject331;
+
 
 public class Contractor extends Customer 
 {
     private static int contractorCount = 6000;
     private int contractorID;
-    public String contractingName; 
+    public String contractorName; 
     public long contractorNumber; 
+    public String notes; 
     Customer firstName; 
     
-public Contractor (String firstName, String lastName, String street, String city, 
-        String state, int zip, String contractingName, long contractorNumber)
+public Contractor (String contractorName, String city, String state, 
+        String street, int zip, long contractorNumber, String notes)
     {
-        super(firstName, lastName, street, city, state, zip, contractorNumber);
-        this.contractingName = contractingName;
-        this.contractorNumber = contractorNumber;
+        super(city, state, street, zip, contractorNumber, notes);
+        this.contractorName = contractorName;
+        this.phoneNumber = contractorNumber; 
         this.contractorID = contractorCount;
+        this.description = notes;
         contractorCount++;
     }
     
+
     //setting the contractor company name
-    public void setContractorName (String contractingName) 
+    public void setContractorName (String contractorName) 
     {
-       this.contractingName = contractingName; 
+       this.contractorName = contractorName; 
     }
- 
+    public String setContractorAddress( String contractingAddress)
+    {
+        contractingAddress ="";
+        contractingAddress += this.street + ", "; 
+        contractingAddress += this.city + ", ";
+        contractingAddress += this.state + ", ";
+        contractingAddress += this.zip;
+        return contractingAddress; 
+    }
     
     //setting contractor company number
     public void setContractorNumber (long contractorNumber) 
